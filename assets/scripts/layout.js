9d3cf9e20a28e679dd5a882b80d42561c0dca27a
tomasmotadev <tomasmotadev@proton.me>
2026-05-21 19:07:12 +0100

    May 21, 2026

--- assets/scripts/layout.js
+++ assets/scripts/layout.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function () {
           </div>
         </li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle">Regulamento / Preços</a>
+          <a href="#" class="dropdown-toggle">Incrição</a>
           <div class="dropdown-menu">
             <a href="${url('/regulamento')}" target="_blank">Regulamento</a>
             <a href="${url('/inscricoes')}" target="_blank">Inscrever</a>
